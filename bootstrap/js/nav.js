8f03727c1d0449c1eee1ef086998bc11da3889bf
Your Name <you@example.com>
2023-11-25 11:28:57 +0700

    edit

--- bootstrap/js/nav.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar-sticky-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
